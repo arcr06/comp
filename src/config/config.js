3b46a4385ef2f8da184517189d36ce30a6b2f1ad
arcr06 <39128000+arcr06@users.noreply.github.com>
2018-11-22 07:41:26 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,1 @@
-export const config = {
-    apiKey: "AIzaSyCIp50Q5bBtj_I2P9Bve6ENa94Oak4hywc",
-    authDomain: "comp-push.firebaseapp.com",
-    databaseURL: "https://comp-push.firebaseio.com",
-    projectId: "comp-push",
-    storageBucket: "comp-push.appspot.com",
-    messagingSenderId: "711039652461"
-  };
+
